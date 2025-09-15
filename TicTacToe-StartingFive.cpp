d900dcc91a6dc76f9c62714b65cd179dc84f0519
Skimlk <vanhoutencocoapowder@gmail.com>
2025-09-15 16:57:52 -0400

    START5-29: Create and call printTitleHeader function

--- TicTacToe-StartingFive.cpp
+++ TicTacToe-StartingFive.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+using namespace std;
+
+void printTitleHeader() {
+    cout << R"(  _______ _   _______      _______         )" << endl;
+    cout << R"( |__   __(_) |__   __|    |__   __|        )" << endl;
+    cout << R"(    | |   _  ___| | __ _  ___| | ___   ___ )" << endl;
+    cout << R"(    | |  | |/ __| |/ _` |/ __| |/ _ \ / _ \)" << endl;
+    cout << R"(    | |  | | (__| | (_| | (__| | (_) |  __/)" << endl;
+    cout << R"(    |_|  |_|\___|_|\__,_|\___|_|\___/ \___|)" << endl;
+    cout << endl;
+    cout << "Created by: Brendan, John, Mike, Philip, and Sam" << endl;
+    cout << endl;
+}
 
 int main()
 {
-    std::cout << "Hello TicTacToe!\n";
+    printTitleHeader();
 }
