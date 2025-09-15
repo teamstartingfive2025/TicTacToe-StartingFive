2eaf83ad0d79824802ba3914ce3c4827319fe2f9
John Giglio <john.p.giglio@gmail.com>
2025-09-15 18:32:56 -0400

    Initialized and stubbed Board.cpp, stubbed Board.h

---
+++ Board.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "Board.h"
+
+Board::Board() {
+	reset();
+}
+
+void Board::reset() {
+	/*spaces = {
+		{'\0','\0','\0'},
+		{'\0','\0','\0'},
+		{'\0','\0','\0'}
+	};
+	return;*/
+}
+
+bool Board::updateSpace(int y, int x, char c) {
+	// Verify space is within bounds. If not print appropriate error message and return false
+	// Verify space is empty. If not print appropriate error message and return false
+	// Change space as specified
+	// Return true
+}
+
+bool Board::checkWin(int y, int x) {
+	//check if the given piece at the given location has resulted in a win; yes: return true, no: return false
+	reset();
+}
--- Board.h
+++ Board.h
@@ -1,28 +1,20 @@
 #pragma once
+#ifndef BOARD_H
+
+#define BOARD_H
+
 class Board {
 private:
 	char spaces[3][3];
 
-
 public:
-	void reset() {
-		/*spaces = {
-			{'\0','\0','\0'},
-			{'\0','\0','\0'},
-			{'\0','\0','\0'}
-		};
-		return;*/
-	}
 
-	bool updateSpace(int y, int x, char c) {
-		// Verify space is within bounds. If not print appropriate error message and return false
-		// Verify space is empty. If not print appropriate error message and return false
-		// Change space as specified
-		// Return true
-	}
+	Board();
 
-	bool checkWin(int y, int x) {
-		//check if the given piece at the given location has resulted in a win; yes: return true, no: return false
-		reset();
-	}
+	void reset();
+
+	bool updateSpace(int y, int x, char c);
+
+	bool checkWin(int y, int x);
 };
+#endif
