5907c1b7eec9a507bab884ad0ea07ce6a597e80e
John Giglio <john.p.giglio@gmail.com>
2025-09-15 18:04:25 -0400

    initialized Board.h

---
+++ Board.h
@@ -0,0 +1,16 @@
+#pragma once
+class Board {
+	private:
+		char spaces[3][3];
+
+
+	public:
+		void reset() {
+			/*spaces = {
+				{'\0','\0','\0'},
+				{'\0','\0','\0'},
+				{'\0','\0','\0'}
+			};
+			return;*/
+		}
+}
